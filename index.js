c64782bc41eb545682b523c34f5d34cb9079656f
peoplestealusernames <wintercar101@gmail.com>
2021-09-25 00:59:47 -0500

    test

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res)=>{
-    res.json("Hello");
+app.get("/", (req, res) =>
+{
+    res.json("Hello1");
 });
 
-app.get("/hello", (req, res)=>{
+app.get("/hello", (req, res) =>
+{
     res.json("World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () =>
+{
     console.log("Listening on port " + PORT)
 });
